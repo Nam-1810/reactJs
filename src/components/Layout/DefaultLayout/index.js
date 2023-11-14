3cb12654519f931595d64fb3bdd7189e0b305406
Nguyen Nam <65749694+Nam-1810@users.noreply.github.com>
2023-11-14 22:09:59 +0700

    Update session 2

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,17 +1,22 @@
-import Header from "./Header";
+import classNames from "classnames/bind";
+import styles from './DefaultLayout.module.scss'
+import Header from "~/components/Layout/components/Header";
 import Sidebar from "./Sidebar";
 
-function DefaultLayout({ children }) {
-    return <div>
-        <Header></Header>
-        <div className="container">
-            <Sidebar></Sidebar>
-            <div className="content">
-                {children}
-            </div>
-        </div>
+const cx = classNames.bind(styles)
 
-    </div>;
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header></Header>
+            <div className={cx('container')}>
+                <Sidebar></Sidebar>
+                <div className={cx('content')}>
+                    {children}
+                </div>
+            </div>
+
+        </div>)
 }
 
 export default DefaultLayout;
