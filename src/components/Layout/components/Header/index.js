8799468da7f85f8e7a7dd292d8a71559347fbffb
Nguyen Nam <65749694+Nam-1810@users.noreply.github.com>
2023-11-15 00:05:47 +0700

    Update session 3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,29 @@
 import classNames from "classnames/bind";
 import styles from './Header.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles)
 
 function Header() {
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
-sss
-        </div>
-    </header>;
+            <div className={cx('logo')}>
+                <h1>Logo</h1>
+            </div>
+            <div className={cx('search')}>
+                <input placeholder="Search account and video...." spellCheck='false' />
+                <button className={cx('clear')}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className="action"></div>
+        </div >
+    </header >;
 }
 
 export default Header;
